1287b775e1a0b4cf89ccd1882a94cc6605602036
Me1oun <dvorak.honza8@gmail.com>
2017-11-28 18:35:10 +0100

    update

--- token.h
+++ token.h
@@ -47,52 +47,102 @@
 #define FLOAT_V   	52
 #define STRING_V    53
 #define ASSIGN      54  //=
-//#define EQ          55  //==
-#define NEQ         56  //<>
-#define LESS        57  //<
-#define GREAT       58  //>
-#define LESSEQ      59  //<=
-#define GREATEQ     60  //>=
-#define ADD         61  //+
-#define SUB         62  //-
-#define MUL         63  //*
-#define DIV         64  //"/"
-#define INTDIV      65  //"\"
-#define EOL			66	//'\n' - end of line
-#define BRACKET_L	67  // (
-#define BRACKET_R 	68  // )
-#define SEMICOLON	69 	// ;
-#define COLON		70  // ,
+#define NEQ         55  //<>
+#define LESS        56  //<
+#define GREAT       57  //>
+#define LESSEQ      58  //<=
+#define GREATEQ     59  //>=
+#define ADD         60  //+
+#define SUB         61  //-
+#define MUL         62  //*
+#define DIV         63  //"/"
+#define INTDIV      64  //"\"
+#define EOL			65	//'\n' - end of line
+#define BRACKET_L	66  // (
+#define BRACKET_R 	67  // )
+#define SEMICOLON	68 	// ;
+#define COLON		69  // ,
 //#define EOF -1
 //my preciouses
-#define EXCL_M		71	// !
-#define EXPONENT	72  //float value with exponent
-#define L_COMMENT	89  //in line comment
-#define B_COMMENT	90	//in block commnet
+#define EXCL_M		70	// !
+#define EXPONENT	71  //float value with exponent
 #define SCAN_ERR	666 //lexem error
+
+//expansion
+#define AMP			72  //&
+#define BINARY		73	//int in binary
+#define	OCTAL		74  //int in octal
+#define HEXA		75	//int in hexadecimal
 
 
 #ifndef TTOKEN
+
+/**
+ * @brief Struktura reprezentujici jeden token.
+ */
+
 typedef struct token{
-	int type;
+	int type;		/**< Urcuje typ tokenu.*/
 	
-	int int_v;
-	double float_v;
-    char *string;
+	int int_v;		/**< Pokud je token typu integer, hodnota se ulozi do int_v.*/
+	double float_v;	/**< Pokud je token typu double, hodnota se ulozi do float_v.*/
+    char *string;	/**< Pokud je token typu id nebo string , hodnota se ulozi do *string.*/
 
 }TToken;
 #endif
 #define TTOKEN
 
+/**
+ * @brief Alokuje misto pro token a inicalizuje jeho cleny.
+ * 
+ * @return Vraci adresu na inicializovany token.
+ */ 
 TToken* token_init();
 
+/**
+ * @brief Smaze token.
+ * 
+ * Pokud je clen tokenu *string ruzny od NULL uvolni alokovanou pamet. Pote uvolni pamet pro
+ * celou celou strukturu token.
+ * 
+ * @param t Ukazatel na token, ktery bude uvolnen.
+ */
 void token_free(TToken* t);
 
+/**
+ * @brief Prochazi zdrojovym souborem a vrati dalsi token.
+ * 
+ * TODO
+ * 
+ * @param t Ukazatel na token, 
+ * @param s Zasobnik na uschovu znaku, pro nasledne ulozeni do string hodnoty tokenu.
+ * @param storage Ukazatel na integer, ve kterem je uschovan znak z predesleho volani funkce, ktery byl precten, ale nezpracovan.
+ * 
+ * @return Vraci ukazatel s aktualizovanym tokenem.
+ */
 TToken *get_next(TToken *t, Tstack* s, int *storage);
 
+/**
+ * @brief Vyhodnocuje zda je zadana escape sekvence uvnitr retezce validni.
+ * 
+ * @param s Zasobnik, na ktery bude ulozena validni escape sekvence.
+ * 
+ * @return Vraci jednicku pokud je escape sekvence validni a nulu v opacnem pripade.
+ */
 int valid_ES(Tstack* s);
 
-//vraci posledni nacteny znak tedy EOL
+/**
+ * @brief Preskoci radkovy komentar.
+ * 
+ * @param state Pokud narazi na EOF, tak zmeni stav na EOF.
+ * 
+ * @return Vraci posledni nacteny znak, tedy LF.
+ */
 int skip_LC(int *state);
 
+/**
+ * @brief Preskoci blokovy komentar.
+ * 
+ * @param state Pokud je komentar spatne zadany meni @p state na SCAN_ERR.
+ */
 void skip_BC(int *state);
